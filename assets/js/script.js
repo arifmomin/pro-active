fa610b746222e2bd3d7a44885ee285fe237fd115
Arif Momin <148381072+arifmomin@users.noreply.github.com>
2023-12-30 20:37:29 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+// #INSIDE OUR APP -> Slider
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+
+// #WHAT OUR CLIENTS SAY’S Slider
+var swiper = new Swiper(".client_says_slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+
+
